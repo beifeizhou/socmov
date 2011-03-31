b8c4eaa81a421a323b50432302179b354c38297b
pratik <pratik@pratik-laptop.(none)>
2011-04-01 01:07:59 +0530

    Added installer script

---
+++ installer.sh
@@ -0,0 +1,31 @@
+#Base installation
+sudo apt-get install git-core
+sudo apt-get install python
+
+#The Movie Database Python SDK Installation
+git clone https://github.com/doganaydin/themoviedb.git
+cd themoviedb
+sudo python setup.py install
+cd ..
+
+#Facebook Python SDK Installation
+git clone https://github.com/facebook/python-sdk.git
+cd python-sdk
+sudo python setup.py install
+cd ..
+
+#PyMVPA installation
+git clone https://github.com/PyMVPA/PyMVPA.git
+sudo apt-get install python-setuptools
+sudo apt-get install swig
+sudo apt-get install python-dev
+cd PyMVPA
+sudo python setup.py install
+cd ..
+
+#Django installation
+wget http://media.djangoproject.com/releases/1.3/Django-1.3.tar.gz
+tar xzvf Django-1.3.tar.gz
+cd Django-1.3
+sudo python setup.py install
+cd ..
