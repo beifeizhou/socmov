6991020dd7ff07857eabfbd2ec1443cf4907287d
pratik <pratik@pratik-laptop.(none)>
2011-04-02 18:44:03 +0530

    Updated the User model by (batch) fetching user-data. TODO, actually implement caching - low priority

--- fbcon/models.py
+++ fbcon/models.py
@@ -1,5 +1,12 @@
+
 from django.db import models
 from themoviedb import tmdb
+from datetime import *	
+
+import urllib
+import urllib2
+import json
+_parse_json = lambda s: json.loads(s)
 
 class Genre(models.Model):
 	gid = models.IntegerField()
@@ -43,6 +50,8 @@ class Movie(models.Model):
 	last_modified_by_tmdb = models.DateField()
 	released = models.DateField()
 	last_modified_by_us = models.DateField()
+	
+	#foreign key(s)
 	genre = models.ManyToManyField("Genre")
 	
 	#json encoded fields
@@ -51,10 +60,11 @@ class Movie(models.Model):
 	keywords = models.TextField()
 	studios = models.TextField()
 	countries = models.TextField()
-		
+
+	#TODO: puravshah, caching
 	def getMovieById(self, mid):
 		return tmdb.getMovieInfo(mid)	
-		
+	
 	def browse(self, order_by, order, topX, gen):
 		url = "http://api.themoviedb.org/2.1/Movie.browse/en-US/xml/" + tmdb.config['apikey']
 		url += "?order_by=" + str(order_by) + "&order=" + str(order) + "&page=1&per_page=" + str(topX)
@@ -86,22 +96,88 @@ class Movie(models.Model):
 		return movie
 	
 class User(models.Model):
-	username = models.CharField(max_length=128)
-	first_name = models.CharField(max_length=128)
-	last_name = models.CharField(max_length=128)
-	gender = models.CharField(max_length=16)
-	uid = models.IntegerField()
-	hometown = models.CharField(max_length=2048)
-	languages = models.CharField(max_length=2048)
+	username = models.CharField(max_length=128, null=True)
+	first_name = models.CharField(max_length=128, null=True)
+	last_name = models.CharField(max_length=128, null=True)
+	gender = models.CharField(max_length=16, null=True)
+	uid = models.CharField(max_length=128, primary_key=True)
+	hometown = models.CharField(max_length=2048, null=True)
+	languages = models.CharField(max_length=2048, null=True)
 	link = models.CharField(max_length=128)
-	interested_in = models.CharField(max_length=1024)
-	relationship_status = models.CharField(max_length=1024)
-	religion = models.CharField(max_length=1024)
+	interested_in = models.CharField(max_length=1024, null=True)
+	relationship_status = models.CharField(max_length=1024, null=True)
+	religion = models.CharField(max_length=1024, null=True)
 	photo_url = models.CharField(max_length=1024)
-	
+
 	#json encoded strings
 	friends = models.TextField()
 	movies = models.TextField()
-	
-	
 
+	
+	last_fetched = models.DateTimeField()
+	
+	
+	def fetch(id, access_token):
+		id = str(id)
+		batchstr  = [{"method":"get","relative_url":id,"access_token":access_token},
+					 {"method":"get","relative_url":id+"/friends","access_token":access_token},
+					 {"method":"get","relative_url":id+"/movies","access_token":access_token}]
+		args = { "batch" : batchstr,
+				 "method" : "POST", 
+				 "access_token" : access_token, 
+				}
+		file = urllib2.urlopen("https://graph.facebook.com/", urllib.urlencode(args))
+		resp = _parse_json(file.read())
+		
+		profile = _parse_json(resp[0]['body'])
+		friends = _parse_json(resp[1]['body'])['data']
+		likes = _parse_json(resp[2]['body'])['data']
+		
+		x = User( 	username = profile.get('username'),
+					first_name = profile.get('first_name'),
+					last_name = profile.get('last_name'),
+					gender = profile.get('gender'),
+					uid = profile.get('id'),
+					hometown = profile.get('hometown'),
+					languages = profile.get('languages'),
+					link = profile.get('link'),
+					interested_in = profile.get('interested_in'),
+					relationship_status = profile.get('relationship_state'),
+					religion = profile.get('religion'),
+					photo_url = 'http://todo.com',
+					friends = json.dumps(friends),
+					movies = json.dumps(likes),
+					last_fetched = datetime.now(),
+				 )
+		return x
+	fetch = staticmethod(fetch)
+	
+	def fetch_current(access_token):
+		#check cookie for UID, if it exists then fetch for that user, else fetch for another one
+		#security concerns, think about it
+		return User.fetch("me", access_token)
+	fetch_current = staticmethod(fetch_current)
+	
+	
+	def get_current(access_token):
+		#caching / use cookies maybe needed here
+		u = User.fetch_current(access_token)
+		u.save()
+		return u
+	get_current = staticmethod(get_current)
+	
+	def get_by_id(id):
+		r = User.objects.filter(uid=id)
+		cached = True
+		if len(r) == 0:
+			cached = False
+		else:
+			r = r[0]
+			if r.last_fetched < datetime.now() - timedelta(days=1):
+				cached = False
+			else:
+				return r
+		r = fetch(id)
+		r.save()
+		return r
+	get_by_id = staticmethod(get_by_id)
--- fbcon/views.py
+++ fbcon/views.py
@@ -5,12 +5,14 @@ from django.shortcuts import render_to_response
 import urllib2
 import urllib
 import json
-_parse_json = lambda s: json.loads(s)
 
 import facebook
+import settings
 
-FACEBOOK_API_KEY='188365901207571'    		# change to your facebook api key
-FACEBOOK_SECRET_KEY='f09191489ff3faff4d3c056838f6339b' 	# change to your facebook app secret key
+from fbcon.models import *
+
+FACEBOOK_API_KEY=settings.FACEBOOK_API_KEY    		# change to your facebook api key
+FACEBOOK_SECRET_KEY=settings.FACEBOOK_SECRET_KEY 	# change to your facebook app secret key
 
 def index(request):
 	return render_to_response('login.html')
@@ -23,26 +25,24 @@ def show(request):
 	profile = {}
 	friends = []
 	likes = []
-	#print request.COOKIES
+	print request.COOKIES
 	if cookie:
 		# Store a local instance of the user data so we don't need
 		# a round-trip to Facebook on every request
-		#print cookie, " kemcho"
-		graph = facebook.GraphAPI(cookie["access_token"])
+		x = User.get_current(cookie["access_token"])
+		#graph = facebook.GraphAPI(cookie["access_token"])
 		#make a batch request
-		
 		#profile = graph.get_object("me")
 		#friends = graph.get_connections("me", "friends")
 		#likes = graph.get_object("me/movies")
-		
+		"""
 		batchstr  = [{"method":"get","relative_url":"me","access_token":cookie["access_token"]},
 					 {"method":"get","relative_url":"me/friends","access_token":cookie["access_token"]},
 					 {"method":"get","relative_url":"me/movies","access_token":cookie["access_token"]}]
 		args = { "batch" : batchstr,
 				 "method" : "POST", 
-				 "access_token" : cookie["access_token"],
-			   }
-		
+				 "access_token" : cookie["access_token"], 
+				}
 		
 		file = urllib2.urlopen("https://graph.facebook.com/", urllib.urlencode(args))
 		resp = _parse_json(file.read())
@@ -51,10 +51,9 @@ def show(request):
 		friends = _parse_json(resp[1]['body'])['data']
 		likes = _parse_json(resp[2]['body'])['data']
 		
-		
-		
 		print type(profile), type(friends), type(likes)
 		#check on the development console
-	
+		"""
+	else :
+		friends = "Please log in :)"
 	return render_to_response('show.html', {"user": profile, "friends"  : friends, "likes" : likes} )
-
