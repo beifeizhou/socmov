a0a919b1d40aacde23831f56a8102fe796554bd8
pratik <pratik@pratik-laptop.(none)>
2011-04-17 20:49:34 +0530

    Added installer to add new repos, so nothing is missed out on

--- installer.sh
+++ installer.sh
@@ -1,12 +1,12 @@
+cp /etc/apt/sources.list /etc/apt/sources.list.backup
+cat repos.txt >> /etc/apt/sources.list
+apt-get update
+
 mkdir install
 cd install
 
 #Base installation
-sudo apt-get install git-core
-sudo apt-get install python
-sudo apt-get install python-setuptools
-sudo apt-get install swig
-sudo apt-get install python-dev
+sudo apt-get install git-core python python-setuptools swig python-dev mysql-server python-mysqldb 
 
 #The Movie Database Python SDK Installation
 git clone https://github.com/doganaydin/themoviedb.git
@@ -21,16 +21,16 @@ sudo python setup.py install
 cd ..
 
 #numpy
-sudo apt-get install python-numpy
+#sudo apt-get install python-numpy
 
 #scipy
-sudo apt-get install python-scipy
+#sudo apt-get install python-scipy
 
 #PyMVPA installation
-git clone https://github.com/PyMVPA/PyMVPA.git
-cd PyMVPA
-sudo python setup.py install
-cd ..
+#git clone https://github.com/PyMVPA/PyMVPA.git
+#cd PyMVPA
+#sudo python setup.py install
+#cd ..
 
 #Django installation
 wget http://media.djangoproject.com/releases/1.3/Django-1.3.tar.gz
@@ -39,9 +39,14 @@ cd Django-1.3
 sudo python setup.py install
 cd ..
 
-#MySQL Installation
-sudo apt-get install mysql-server
-sudo apt-get install python-mysqldb 
-
 #Create MySQL Database
 mysql -u root -p -e "CREATE DATABASE IF NOT EXISTS socmovdb;"
+
+#wget http://www.csie.ntu.edu.tw/~cjlin/libsvm/libsvm-3.1.tar.gz
+#tar xzvf libsvm-3.1.tar.gz
+#cd libsvm-3.1
+#make
+
+#sudo apt-get install libsvm-tools
+#sudo apt-get install libsvm-dev
+#sudo apt-get install python-libsvm
