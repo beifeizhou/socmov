290a4722da0223f66037ee3848df36667ae2308e
puravshah <puravshah89@gmail.com>
2011-05-07 17:22:03 +0530

    FB friends like bug fixed

--- fbcon/models.py
+++ fbcon/models.py
@@ -1,4 +1,4 @@
-from django.db import models, connection
+from django.db import models, connection, transaction
 from datetime import *	
 import urllib
 import urllib2
@@ -430,25 +430,40 @@ class User(models.Model):
 	def add_movie(self, mov, rating):
 		if mov == None:
 			return
-		a = Vote.objects.filter(movie__mid = mov.mid).filter(user__uid = self.uid)
-		if len(a) > 0:
-			a.delete()
+		cursor = connection.cursor()
+		cursor.execute('DELETE FROM fbcon_vote WHERE movie_id = %s and user_id = %s', [mov.mid, self.uid])
+		transaction.commit_unless_managed()
+		#Vote.objects.filter(movie__mid = mov.mid).filter(user__uid = self.uid).delete()
+		#if len(a) > 0:
+			#a.delete()
 		v = Vote(movie = mov, user = self, last_updated = datetime.now(), rating = rating)
 		v.save()
 	
 	""" get_friends_like function is used to find all the friends which like the given Movie mov """	
 	def get_friends_who_like(self, mov):
 		fr = self.get_friends()
-		ids = []
+		ids = [mov.mid]
 		for f in fr:
 			ids.append(f['id'])
-			
-		ret = User.objects.filter(uid__in = ids).filter(movie__mid = mov.mid).filter(vote__rating = 1)
-		return ret
+		
+		ans = []
+		query = "SELECT * FROM fbcon_user, fbcon_vote, fbcon_movie WHERE fbcon_vote.movie_id = fbcon_movie.mid and fbcon_user.uid = fbcon_vote.user_id and fbcon_movie.mid = %s and fbcon_vote.rating = 1 and uid in (%s"
+		for i in range(2, len(ids)):
+			query = query + ", %s"
+		query = query + ")"
+		ret = User.objects.raw(query, ids)
+		for i in ret:
+			ans.append(i)
+		#ret = User.objects.filter(uid__in = ids).filter(vote__movie = mov).filter(vote__rating = 1)
+		return ans
 		
 	def get_movie_likes(self):
-		ret = Movie.objects.filter(vote__user = self).filter(vote__rating = 1)
-		r = []
+		ret = Movie.objects.raw('SELECT * FROM fbcon_movie WHERE mid in (SELECT mid from fbcon_movie, fbcon_user, fbcon_vote where fbcon_vote.user_id = %s and fbcon_vote.movie_id = fbcon_movie.mid and fbcon_user.uid = fbcon_vote.user_id and fbcon_vote.rating = 1)', [self.uid])
+		ans = []
+		for i in ret:
+			ans.append(i)
+		#ret = Movie.objects.filter(user__uid = self.uid).filter(vote__rating = 1)
+		"""r = []
 		i = 0
 		while i < len(ret):
 			r.append(ret[i])
@@ -456,7 +471,9 @@ class User(models.Model):
 			while j < len(ret) and ret[j].mid == ret[i].mid:
 				j = j + 1
 			i = j
-		return r
+		return r"""
+		return ans
+		
 	def get_age(self):
 		if self.birthday == None:
 			return None
